4b786441a1938561d5f16e19fc28cc7c2c83a102
maksymfedunov <maksim.kherson88@gmail.com>
2025-12-30 16:53:54 +0100

    Сохранил прогресс перед обновлением

---
+++ PhotoShare/src/conf/config.py
@@ -0,0 +1,20 @@
+from typing import Any
+
+from pydantic import ConfigDict, field_validator, EmailStr
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    DB_URL: str = "postgresql+asyncpg://postgres:098765@localhost:5432/abcdef"
+    SECRET_KEY_JWT: str = "1234567890"
+    ALGORITHM: str = "HS256"
+    
+
+    
+
+    model_config = ConfigDict(
+        extra="ignore", env_file=".env", env_file_encoding="utf-8"
+    )
+
+
+config = Settings()
