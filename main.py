435193af0c6ec443b4084350682575b441026f08
Magnteo <bogdanserg48@gmail.com>
2026-01-01 21:49:05 +0200

    User Profiles Admin Features

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Запуск FastAPI
 from fastapi import FastAPI
-from src.routes import photos, users, auth  
+from src.routes import users, auth, admin , photo
 
 
 
@@ -8,4 +8,5 @@ app = FastAPI()
 
 app.include_router(auth.router, prefix="/api")
 app.include_router(users.router, prefix="/api")
-app.include_router(photos.router, prefix="/api") 
+app.include_router(admin.router, prefix="/api") 
+app.include_router(photo.router, prefix="/api") 
--- src/routes/users.py
+++ src/routes/users.py
@@ -1,15 +1,56 @@
-from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends , HTTPException
+
 from src.services.auth import auth_service
-from src.entity.models import RoleEnum
+from src.entity.models import RoleEnum , User
 from src.services.dependencies import role_required
+from src.schemas.user import PublicProfile, PrivateProfile ,UserUpdate
+from src.database.db import get_db
+from src.repository import users as repository_users
 
-
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 router = APIRouter(prefix="/users", tags=["users"])
 
 @router.get("/admin-only")
 async def admin_panel(user=Depends(role_required(RoleEnum.admin))):
     return {"msg": f"Hello, admin {user.username}!"}
 
-@router.get("/me")
-async def read_my_profile(user=Depends(auth_service.get_current_user)):
-    return {"email": user.email, "username": user.username, "role": user.role}
+
+
+@router.get("/{username}", response_model=PublicProfile)
+async def public_profile(
+    username: str,
+    db: AsyncSession = Depends(get_db),
+):
+    result = await db.execute(
+        select(User).where(User.username == username, User.is_banned == False)
+    )
+    user = result.scalar_one_or_none()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="user not found")
+
+    return {
+        "username": user.username,
+        "full_name": user.full_name,
+        "created_at": user.created_at,
+        "photos_count": len(user.photos),
+    }
+
+@router.get("/profile/me", response_model=PrivateProfile)
+async def my_profile(
+    current_user: User = Depends(auth_service.get_current_user),
+):
+    return current_user
+
+@router.put("/me", response_model=PrivateProfile)
+async def update_my_profile(
+    data: UserUpdate,
+    current_user: User = Depends(auth_service.get_current_user),
+    db: AsyncSession = Depends(get_db),
+):
+    updated_user = await repository_users.update_user(
+        current_user, data, db
+    )
+    return updated_user
