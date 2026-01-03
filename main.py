8740159ceea0141ca339b5a001c88f521b7943aa
Magnteo <bogdanserg48@gmail.com>
2026-01-03 18:07:24 +0200

    переніс photo в photos

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Запуск FastAPI
 from fastapi import FastAPI
-from src.routes import users, auth, admin , photo, profile
+from src.routes import users, auth, admin , photos, profile
 
 
 
@@ -9,9 +9,6 @@ app = FastAPI()
 app.include_router(auth.router, prefix="/api")
 app.include_router(users.router, prefix="/api")
 app.include_router(admin.router, prefix="/api") 
-
-app.include_router(photo.router, prefix="/api") 
 app.include_router(profile.router, prefix="/api") 
-
 app.include_router(photos.router, prefix="/api") 
->>>>>>> main
+
--- src/routes/photos.py
+++ src/routes/photos.py
@@ -5,9 +5,19 @@ from src.database.db import get_db
 from src.services.auth import auth_service
 from src.services.photos import assign_tags_to_photo, get_owned_photo, upload_photo_service
 from src.repository.photos import create_photo, get_photo, update_photo, delete_photo
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.repository import photos as repository_photos
 
 router = APIRouter(prefix="/photos", tags=["photos"])
 
+
+
+@router.get("/{photo_id}")
+async def get_photo(photo_id: int, db: AsyncSession = Depends(get_db)):
+    photo = await repository_photos.get_photo_by_id(photo_id, db)
+    if not photo:
+        raise HTTPException(status_code=404, detail="Photo not found")
+    return photo
 
 @router.post("/", response_model=PhotoResponse)
 async def upload_photo(
