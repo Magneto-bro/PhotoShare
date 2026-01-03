da27d51bb28a7b8446c070190a2deda00867ee2e
Magnteo <bogdanserg48@gmail.com>
2026-01-03 17:38:05 +0200

    переніс всі операції з профілем в окремий файл

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Запуск FastAPI
 from fastapi import FastAPI
-from src.routes import users, auth, admin , photo
+from src.routes import users, auth, admin , photo, profile
 
 
 
@@ -10,3 +10,4 @@ app.include_router(auth.router, prefix="/api")
 app.include_router(users.router, prefix="/api")
 app.include_router(admin.router, prefix="/api") 
 app.include_router(photo.router, prefix="/api") 
+app.include_router(profile.router, prefix="/api") 
